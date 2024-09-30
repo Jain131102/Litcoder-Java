915cf190bb0f719b8f14251b1cd88074fcef0c0d
Sourabh Jain <126811697+Jain131102@users.noreply.github.com>
2024-09-30 15:11:11 +0530

    For CS_JAVA_Familiarization Lab

---
+++ Multiply 2 numbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int ret = mul(a,b);        
+        System.out.println(ret);
+    }
+    
+    public static int mul(int x, int y) {
+        //Hint : Type return x * y below
+        return x * y;
+    }
+}
+                                                                                                                            
